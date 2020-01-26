38157ac1552854a65bc5ffe2ceece95cb1f8ae38
Damian <d.r.willems@gmail.com>
2020-01-26 14:27:01 +1300

    smaller comp and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.style.scss';
+import Directory from '../../components/directory/directory.component.jsx';
+
+const HomePage = () => (
+<div className='homepage'>
+    
+    <Directory></Directory>    
+    
+</div>
+
+);
+
+export default HomePage
