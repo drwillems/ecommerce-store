3fc2ab02a98f6e1dd744dbe5d7474bd79a0807ff
Damian <d.r.willems@gmail.com>
2020-01-27 13:01:27 +1300

    leeer router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+
+
+const HatsPage = () => (
+  <div>
+  <h1>Hatg</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-    <HomePage></HomePage>
-
+    <Switch>
+    <Route exact path='/' component={HomePage}></Route>
+    <Route exact path='/hats' component={HatsPage}></Route>
+    </Switch>
     </div>
       
    
