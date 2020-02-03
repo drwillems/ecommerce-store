4ac97cda881e2ebe255a1db5ee956b554681cf4a
Damian <d.r.willems@gmail.com>
2020-02-03 21:51:17 +1300

    cart dropdown and icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux'; // higer order component
 import {auth} from '../../firebase/firebase.utils';
-
+import CartIcon from '../cart-icon/cart-icon.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({hidden, currentUser}) => (
     <div className='header'>
     <Link className='logo-container' to='/'>
     <Logo className='logo'/>
@@ -18,12 +20,18 @@ const Header = ({currentUser}) => (
         <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
         :
         (<Link className='option' to='/signin'>SIGN IN</Link>)}
+        <CartIcon/>
     </div>
+    {
+        hidden ? null :
+    <CartDropdown/>
+    }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 // connect is a higer order component
